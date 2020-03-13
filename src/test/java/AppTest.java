90efdb0abd6583ba16c3bd8ab50f0911569beb0e
Fredy <fredy.haryanto@online-pajak.com>
2020-03-13 10:51:57 +0800

    init java project with junit and assertj

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,9 @@
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void testShouldWork() {
+        Assertions.assertThat(true).isTrue();
+    }
+}
