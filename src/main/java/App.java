84a64cf7d062618957705fbb0221b1625afef77b
Sheldon Lim <sheldon.lim@online-pajak.com>
2020-03-13 16:39:39 +0800

    first commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,30 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class App {
+
+    public static final String START_TAG_PATTERN = "<([^/].*?)>";
+    public static final String END_TAG_PATTERN = "</(.*?)>";
+
+    public String extract(String content) {
+        if (content == "" || !content.contains("<") || !content.contains("/"))
+            return "";
+
+        String startTag = getTag(content, START_TAG_PATTERN);
+        String endTag = getTag(content, END_TAG_PATTERN);
+        if(startTag != endTag) return "";
+
+//        Pattern tagPattern = Pattern.compile()
+        return null;
+    }
+
+    private String getTag(String content, String s) {
+        String tag = null;
+        Pattern startPattern = Pattern.compile(s);
+        Matcher matcher1 = startPattern.matcher(content);
+        if (matcher1.find()) {
+            tag = matcher1.group(1);
+        }
+        return tag;
+    }
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -6,4 +6,46 @@ public class AppTest {
     public void testShouldWork() {
         Assertions.assertThat(true).isTrue();
     }
+
+    @Test
+    public void testShouldTakeInEmptyStringAndReturnEmptyString() {
+        App app = new App();
+        String result = app.extract("");
+        Assertions.assertThat(result).isEqualTo("");
+    }
+
+    @Test
+    public void testShouldReturnEmptyStringIfNoTag() {
+        App app = new App();
+        String result = app.extract("abcd");
+        Assertions.assertThat(result).isEqualTo("");
+    }
+
+    @Test
+    public void testShouldReturnEmptyStringIfNoEndTag() {
+        App app = new App();
+        String result = app.extract("<h1>abcd");
+        Assertions.assertThat(result).isEqualTo("");
+    }
+
+    @Test
+    public void testShouldReturnEmptyStringIfStartAndEndTagIsDifference() {
+        App app = new App();
+        String result = app.extract("<h1>abcd</H1>");
+        Assertions.assertThat(result).isEqualTo("");
+    }
+
+    @Test
+    public void testShouldReturnEmptyStringIfStartAndEndTagIsReverse() {
+        App app = new App();
+        String result = app.extract("</h1>abcd<h1>");
+        Assertions.assertThat(result).isEqualTo("");
+    }
+
+    @Test
+    public void testShouldReturnString() {
+        App app = new App();
+        String result = app.extract("<h1>abcd</h1>");
+        Assertions.assertThat(result).isEqualTo("abcd");
+    }
 }
